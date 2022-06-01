47db6dd088872e68b2348d690fc4ad882ea16166
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-01 12:52:02 +0100

    challenge objects create object with name and age property

--- src/objects.js
+++ src/objects.js
@@ -1,13 +1,17 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+     name: name,
+     age: age,
+   }
+
 };
 
 const getName = object => {
-  // your code here
+  return object.name
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[age];
 };
 
 const hasProperty = (property, object) => {
